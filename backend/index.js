975e3f15b009dbbb8469736305e1a2c3d2b35c51
jplusequalt <jplusequalt@gmail.com>
2022-04-08 08:54:09 -0500

    fixed undefined port

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.get("/api/pokemon/:id", (req, res) => {
   res.json(pokemon)
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
