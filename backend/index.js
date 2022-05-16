7f4cef8ae3ee17c51df7c0ea0c75cff81f3be4a5
jplusequalt <jplusequalt@gmail.com>
2022-05-16 16:19:10 -0500

    pokemon selection is now randomly selected once per day

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,18 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const cors = require("cors")
+const randGen = require("random-seed")
 const pokemonJSON = require("./pokemon.json")
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static(__dirname + "/public/"))
 
-app.get("/api/pokemon/:id", (req, res) => {
-  const id = +req.params.id
-  const pokemon = pokemonJSON.pokemon.find(obj => obj.number === id)
+app.get("/api/pokemon", (req, res) => {
+  let date = new Date()
+  const gen = randGen.create(new Date(date.getFullYear(), date.getMonth(), date.getDay()))
+  const index = gen(151) + 1
+  const pokemon = pokemonJSON.pokemon.find(obj => obj.number === index)
   res.json(pokemon)
 })
 
